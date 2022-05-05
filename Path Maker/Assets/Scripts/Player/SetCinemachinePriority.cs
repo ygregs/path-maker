e9f1f7a96dccc5857eb40c94091de3c5e8902740
toutane <ca@leger.email>
2022-05-05 18:48:55 +0200

    do rigging

--- Path Maker/Assets/Scripts/Player/SetCinemachinePriority.cs
+++ Path Maker/Assets/Scripts/Player/SetCinemachinePriority.cs
@@ -6,22 +6,22 @@ using Cinemachine;
 public class SetCinemachinePriority : NetworkBehaviour
 {
 
-    public CinemachineFreeLook cinemachinefreelook;
-    public CinemachineFreeLook aimFreeLook;
+    public CinemachineVirtualCamera cinemachineVirtualLook;
+    // public CinemachineFreeLook aimFreeLook;
     // public GameObject canvas;
 
     public override void OnNetworkSpawn()
     {
         if (IsOwner)
         {
-            cinemachinefreelook.Priority = 10;
-            aimFreeLook.Priority = 11;
+            cinemachineVirtualLook.Priority = 10;
+            // aimFreeLook.Priority = 11;
             // canvas.SetActive(true);
         }
         else
         {
-            cinemachinefreelook.Priority = 0;
-            aimFreeLook.Priority = 0;
+            cinemachineVirtualLook.Priority = 0;
+            // aimFreeLook.Priority = 0;
             // canvas.SetActive(false);
         }
     }
